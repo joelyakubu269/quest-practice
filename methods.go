3e6ee74d22c5b7faf48d49c47b67c53bb51d8e91
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-20 16:01:12 +0100

    learning about methods in go

--- methods.go
+++ methods.go
@@ -1,10 +1,29 @@
-type interface struct {
+package main
+
+type Shape interface {
 	Area() float64
 }
-type Triangle {
-	Base float64
+type Triangle struct {
+	Base   float64
 	Height float64
 }
+type Circle struct {
+	Radius float64
+}
+
 func (p Triangle) Area() float64 {
 	return p.Base * p.Height
 }
+
+type Perimeter interface {
+	Perimeter() float64
+}
+
+func PrintAreas(shapes []Shape) float64 {
+	totalArea := 0.0
+	for _, s := range shapes {
+		totalArea += s.Area()
+	}
+	return totalArea
+}
+func PrintPerimeters(shapes []Shape)
