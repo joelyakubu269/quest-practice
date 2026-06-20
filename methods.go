9f04adedde7b7549b0f4c7e4fa70819c2dbb9e56
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-20 16:13:51 +0100

    learning about methods in go

--- methods.go
+++ methods.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 type Shape interface {
 	Area() float64
 }
@@ -14,6 +16,15 @@ type Circle struct {
 func (p Triangle) Area() float64 {
 	return p.Base * p.Height
 }
+func (p Triangle) Perimeter() float64 {
+	return 2 * (p.Base + p.Height)
+}
+func (p Circle) Area() float64 {
+	return math.Pi * p.Radius * p.Radius
+}
+func (p Circle) Perimeter() float64 {
+	return math.Pi * 2 * p.Radius
+}
 
 type Perimeter interface {
 	Perimeter() float64
@@ -26,4 +37,11 @@ func PrintAreas(shapes []Shape) float64 {
 	}
 	return totalArea
 }
-func PrintPerimeters(shapes []Shape)
+func PrintPerimeters(shapes []Perimeter) float64 {
+	totalPerimeter := 0.0
+	for _, s := range shapes {
+		totalPerimeter += s.Perimeter()
+	}
+	return totalPerimeter
+
+}
